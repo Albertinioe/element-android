e0e11db55dcc0a70443bc5c549cf9c60e322ff0b
19Albert92 <Albert.zinnurov2017@yandex.ru>
2021-12-07 18:50:02 +0100

    убрал не нужную информацию из Help & About

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright 2019 New Vector Ltd
+ * Copyright 2021 Qwerty Networks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +17,16 @@
 
 package im.vector.app.features.settings
 
+import java.util.Locale
+
 object VectorSettingsUrls {
 
+//    val lang = Locale.getDefault().language // язык телефона
+
     const val COPYRIGHT = "https://element.io/copyright"
-    const val TAC = "https://element.io/terms-of-service"
-    const val PRIVACY_POLICY = "https://element.io/privacy"
+    const val  APPLICATION_APP = "https://qaim.me/aboutapp"
+    const val TAC = "https://qaim.me/agreement" //"https://element.io/terms-of-service"
+    const val PRIVACY_POLICY = "https://qaim.me/privacy" //"https://element.io/privacy"
     const val DISCLAIMER_URL = "https://element.io/previously-riot"
     const val THIRD_PARTY_LICENSES = "file:///android_asset/open_source_licenses.html"
 }
